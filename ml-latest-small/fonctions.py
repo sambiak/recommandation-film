aaa6bf991268f2e75c7632cff6e88f73cbee9f45
sambiak <guillaume.augustoni@gmail.com>
2017-02-08 09:28:39 +0100

    Ajout de la fonction moyenne
    Dans le fichier fonctions.py

--- ml-latest-small/fonctions.py
+++ ml-latest-small/fonctions.py
@@ -1,6 +1,20 @@
 from movielens import *
-def note_moyenne():
-    """Fonction qui renvoie la note moyenne"""
-    notes = tableau_des_notes()
-    print(notes)
-note_moyenne()
+def moyenne_des_notes_des_film(fonction):
+    """renvoit la liste des moyennes des notes par film en prenant le tableau des notes"""
+    moy = 0
+    list_moy = []
+    k = 0
+    for i in range(9125):
+        for j in fonction:
+            if not math.isnan(float(j[i])):            
+                moy += j[i]
+                k += 1
+        if k != 0:
+            moy = moy / k
+        else:
+            moy = float('nan')
+        list_moy += [moy]
+    return list_moy
+
+print(moyenne_des_notes_par_film(tableau_des_notes()))
+
