2fc1f40516690b6b9c822e82ee5fd501c9878e39
sambiak <guillaume.augustoni@gmail.com>
2017-02-08 09:27:23 +0100

    Retiré la fonction moyenne
    Déplacé dans la branche non complet dans le fichier algorithmes

--- ml-latest-small/movielens.py
+++ ml-latest-small/movielens.py
@@ -100,21 +100,4 @@ def tableau_des_notes():
     return np.array(array)
 
 
-def moyenne_des_notes_par_film(fonction):
-    """renvoit la liste des moyennes des notes par film en prenant le tableau des notes"""
-    moy = 0
-    list_moy = []
-    k = 0
-    for i in range(9125):
-        for j in fonction:
-            if not math.isnan(float(j[i])):            
-                moy += j[i]
-                k += 1
-        if k != 0:
-            moy = moy / k
-        else:
-            moy = float('nan')
-        list_moy += [moy]
-    return list_moy
 
-print(moyenne_des_notes_par_film(tableau_des_notes()))
