cfbc87866759bb48cc37dcf4499069dd8b95880d
sambiak <guillaume.augustoni@gmail.com>
2017-02-09 13:33:27 +0100

    Fonction moyenne qui marche

--- ml-latest-small/fonctions.py
+++ ml-latest-small/fonctions.py
@@ -1,20 +1,20 @@
 from movielens import *
-def moyenne_des_notes_des_film(fonction):
+import math
+def moyenne_des_notes_des_film(array):
     """renvoit la liste des moyennes des notes par film en prenant le tableau des notes"""
-    moy = 0
     list_moy = []
-    k = 0
+    k=0
+    moy=0
     for i in range(9125):
-        for j in fonction:
-            if not math.isnan(float(j[i])):            
-                moy += j[i]
-                k += 1
-        if k != 0:
-            moy = moy / k
+        for j in (array[:,i]):
+            if not math.isnan(j) :
+                k+=1
+                moy+=j
+        if k==0:
+            list_moy += [math.nan]
         else:
-            moy = float('nan')
-        list_moy += [moy]
+            list_moy += [moy/k]
     return list_moy
 
-print(moyenne_des_notes_par_film(tableau_des_notes()))
+
 
