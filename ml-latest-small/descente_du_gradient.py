f36efe1be537049713f3876d8520036f6f165fd2
Unknown <anthony.giraudo@etu.univ-amu.fr>
2017-03-05 10:56:36 +0100

    amelioration de la fonction ecart absolu et moyenne des ecarts absolus
    la moyenne des ecart absolu renvoi maintenant la moyenne des ecarts absolus qu on trouve en lançant la descente du gradient dans tous les cas possibles sur le sous ensembles

--- ml-latest-small/descente_du_gradient.py
+++ ml-latest-small/descente_du_gradient.py
@@ -64,6 +64,9 @@ print(np.random.random((1, len(tableau[0]))))"""
 
 
 def écart_relatif_en_pourcentage_entre_y_i_et_h_de_x_i_apres_descente_du_gradient(i,sous_ensemble):
+    """
+    fonction qui peut encore etre ameliorée
+    """
     ss_ensemble=list(sous_ensemble)
     x_i=ss_ensemble[i][:-1]
     y_i=ss_ensemble[i][-1]
@@ -76,28 +79,45 @@ def écart_relatif_en_pourcentage_entre_y_i_et_h_de_x_i_apres_descente_du_gradie
     return abs(y_i-h_theta(theta,x_i))/y_i*100
 
 def moyenne_des_ecarts_relatifs(sous_ensemble):
+    """
+    fonction qui peut encore etre amelioree
+    """
     ecart=0
     for i in range(len(sous_ensemble)):
         ecart+=écart_relatif_en_pourcentage_entre_y_i_et_h_de_x_i_apres_descente_du_gradient(i,sous_ensemble)
     return ecart/len(sous_ensemble)
 
 
-def écart_absolu_en_pourcentage_entre_y_i_et_h_de_x_i_apres_descente_du_gradient(i,sous_ensemble):
+def écart_absolu_entre_y_i_et_h_de_x_i_apres_descente_du_gradient(y,i,sous_ensemble):
+    """
+    :param y: numéro de la colonne du tableau extrait que l'on choisi pour etre le resultat (de 0 à len(sous_ensemble-1))
+    :param i: numero de la ligne à laquelle on va appliquer h_theta apres descente du gradient pour comparer l'écart absolu entre cette valeur et y_i
+    :param sous_ensemble: tableau extrait à partir duquel on veut travailler
+    :return: écart absolu entre h_theta(x_i) et y_i apres descente du gradient
+    """
     ss_ensemble=list(sous_ensemble)
-    x_i=ss_ensemble[i][:-1]
-    y_i=ss_ensemble[i][-1]
+    x_i=ss_ensemble[i][:y]+ss_ensemble[i][y+1:]
+    y_i=ss_ensemble[i][y]
     del ss_ensemble[i]
     theta = np.random.random((1, len(ss_ensemble[0])))[0]
-    x=np.array(([x_j[:-1]for x_j in ss_ensemble]))
-    y = np.array([y_i[-1] for y_i in ss_ensemble])
+    x = np.array(([x_j[:y]+x_j[y+1:] for x_j in ss_ensemble]))
+    y = np.array([y_j[y] for y_j in ss_ensemble])
     for i in range(500):
         theta=etape_du_gradient(0.01, theta, x, y)
     return abs(y_i-h_theta(theta,x_i))
 
 def moyenne_des_ecarts_absolus(sous_ensemble):
+    """
+    :param sous_ensemble: tableau extrait sur lequel on veut travailler
+    :return: moyenne de tous les ecarts absolus qu'on peut calculer dans ce tableau
+    cette fonction est trop longue!!!! mais je l ai lancé et à la fin ça me donnait 0.24:en moyenne on predit une note à 0.24 près
+    par contre le max c 3 et quelque je crois c assez enorme
+    """
     ecart=0
-    for i in range(len(sous_ensemble)):
-        ecart+=écart_absolu_en_pourcentage_entre_y_i_et_h_de_x_i_apres_descente_du_gradient(i,sous_ensemble)
-    return ecart/len(sous_ensemble)
+    for y in range(len(sous_ensemble[0])):
+        for i in range(len(sous_ensemble)):
+            #print(écart_absolu_entre_y_i_et_h_de_x_i_apres_descente_du_gradient(y,i,sous_ensemble))
+            ecart+=écart_absolu_entre_y_i_et_h_de_x_i_apres_descente_du_gradient(y,i,sous_ensemble)
+    return ecart/len(sous_ensemble)**2
 
 print(moyenne_des_ecarts_absolus(tableau))
