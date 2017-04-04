7e7038ac066134519136b358c0d75a17cc913575
Unknown <anthony.giraudo@etu.univ-amu.fr>
2017-04-04 22:23:05 +0200

    ajouts de commentaires et modif de la fonction de cout avec des fonction de numpy

--- ml-latest-small/recomendation_system_final.py
+++ ml-latest-small/recomendation_system_final.py
@@ -3,24 +3,39 @@ import math
 from movielens import tableau_des_notes, tableau_bricolé_de_l_lignes_et_c_colonnes
 
 
-def sorte_de_fonction_cout(Y, theta, X):
-    valeur = 0
-    c = 0
-    for i in range (len(Y)):
-        for j in range (len(Y[0])):
-            if not math.isnan(Y[i, j]):
-                c+=1
-                valeur += abs(np.dot(X[j], theta[i].T)-Y[i, j])
-    return valeur/c
+def fonction_cout(Y, theta, X):
+    """
+    :param Y: tableau de notes avec utilisateurs en lignes et films en colonnes
+    :param theta: tableau avec des lignes de profils d'utilisateurs
+    :param X: tableau avec des lignes de caracteristiques de films
+    :return: valeur de la fonction de cout
+    """
+    return np.nanmean((np.dot(X, theta.T) - Y.T) ** 2)
 
 
 def grad_J_par_rapport_a_x_j(Y, x_j, j, theta):
+    """
+    :param Y: tableau de notes avec utilisateurs en lignes et films en colonnes
+    :param x_j: ligne j de X par rapport à laquelle nous calculons les derivees partielles de la fonction de cout
+                (correspond aux caracteristiques du film j)
+    :param j: numero de la ligne de X par rapport à laquelle nous calculons les derivees partielles de la fonction de cout
+    :param theta: tableau avec des lignes de profils d'utilisateurs
+    :return: valeur du gradient de la fonction de cout prise en theta et X par rapport a x_j
+    """
     y_j_tilde = Y.take(j, axis = 1)
     y_j_tilde, theta_tilde = y_j_tilde[~np.isnan(y_j_tilde)], theta[~np.isnan(y_j_tilde)]
     return np.dot(theta_tilde.T, np.dot(theta_tilde, x_j.T) - y_j_tilde).T
 
 
 def grad_J_par_rapport_a_theta_i(Y, theta_i, i, X):
+    """
+    :param Y: tableau de notes avec utilisateurs en lignes et films en colonnes
+    :param theta_i: ligne i de theta par rapport à laquelle nous calculons les derivees partielles de la fonction de cout
+            (correspond aux caracteristiques du film j)
+    :param i: numero de la ligne de theta par rapport à laquelle nous calculons les derivees partielles de la fonction de cout
+    :param X: theta: tableau avec des lignes de profils d'utilisateurs
+    :return: valeur du gradient de la fonction de cout prise en theta et X par rapport a theta_i
+    """
     y_i_tilde = Y.take(i, axis = 0)
     y_i_tilde = y_i_tilde.T
     y_i_tilde, X_tilde = y_i_tilde[~np.isnan(y_i_tilde)], X[~np.isnan(y_i_tilde)]
@@ -28,6 +43,14 @@ def grad_J_par_rapport_a_theta_i(Y, theta_i, i, X):
 
 
 def etape_du_gradient(Y, alpha_X, alpha_theta, theta, X):
+    """
+    :param Y: tableau de notes avec utilisateurs en lignes et films en colonnes
+    :param alpha_X: taux d apprentissage pour la modification de X
+    :param alpha_theta: taux d apprentissage pour la modification de theta
+    :param theta: tableau avec des lignes de profils d'utilisateurs
+    :param X: tableau avec des lignes de caracteristiques de films
+    :return: theta et X modifiés apres une etape de descente du gradient
+    """
 
     n_X = [0] * len(X)
     for j, x_j in enumerate(X):
@@ -41,12 +64,20 @@ def etape_du_gradient(Y, alpha_X, alpha_theta, theta, X):
 
 
 def descente_du_gradient(Y, l, nb_etapes, alpha_X, alpha_theta):
+    """
+    :param Y: tableau de notes avec utilisateurs en lignes et films en colonnes
+    :param l: nombre de caracteristiques de films (nb de colonnes de theta et X)
+    :param nb_etapes: nb d etapes de descente du gradient à realiser
+    :param alpha_X: taux d apprentissage pour la modification de X avec la descente du gradient
+    :param alpha_theta: taux d apprentissage pour la modification de theta avec la descente du gradient
+    :return: theta et X modifiés après nb_etapes d'étapes de descente du gradient
+    """
     X = np.random.random((len(Y[0]), l))
     theta = np.random.random((len(Y), l))
     for etape in range(nb_etapes):
         theta, X = etape_du_gradient(Y, alpha_X, alpha_theta, theta, X)
         print("etape", etape)
-        print(sorte_de_fonction_cout(Y, theta, X))
+        print(fonction_cout(Y, theta, X))
     return theta, X
 
 if __name__ == "__main__":
@@ -77,5 +108,3 @@ print(theta)
 print(" ")
 print(X)
 """
-
-print(np.isnan(tableau_des_notes()).sum())
