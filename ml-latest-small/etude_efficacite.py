49fcda02cacf3d53abbd3d7f66c13dca9bb54024
Yassine Amram <yassine.amram@icloud.com>
2017-04-11 18:47:10 +0200

    Bonne version du fichier
    Début d'optimisation

--- ml-latest-small/etude_efficacite.py
+++ ml-latest-small/etude_efficacite.py
@@ -4,6 +4,21 @@ import math
 from movielens import tableau_des_notes
 from recomendation_system_final import *
 
+def pourcentage_de_non_nan(array): 
+    k=0 
+    for i in range(9125): 
+        for j in array[:,i]: 
+            if not math.isnan(j) : 
+                k+=1 
+    return k/(9125*670) 
+     
+def nbre_de_film_vu_par_utilisateur(array,i): 
+    k=0 
+    for j in array[i,:]: 
+        if not math.isnan(j) : 
+            k+=1 
+    return k 
+
 def notes_extraites_pour_validation(tableau_des_notes_entier):
     """fonction qui
     """
@@ -14,20 +29,22 @@ def notes_extraites_pour_validation(tableau_des_notes_entier):
     
     for n_eme_note_de_validation in range (9922):
         numero_utilisateur = randrange (0,670)
-        numero_film = randrange (1,2391)
+        while nbre_de_film_vu_par_utilisateur(tableau_des_notes_entier,numero_utilisateur) < 10: 
+            numero_utilisateur = randrange (0,670) 
+        numero_film = randrange (1,9125) 
         compteur = [0,0]
-        for i in range(len (T_validation[numero_utilisateur])):
-            if not math.isnan (T_validation[numero_utilisateur][i]):
+        for i, film in enumerate(T_validation[numero_utilisateur]): 
+            if not math.isnan(film): 
                 compteur = [compteur[0]+1,i]
                 if compteur[0] == numero_film:
-                    T_validation[numero_utilisateur][i] = na_n
+                    film = na_n
                     compteur = [0,0]
-                    L_validation += [numero_utilisateur,i]
+                    L_validation.append([numero_utilisateur,i])
                     break
-        if compteur[0]!=0:
-            T_validation[numero_utilisateur][compteur[1]] = na_n
+        if compteur[0] != 0: 
+            T_validation[numero_utilisateur][compteur[1]] = na_n 
+            L_validation.append([numero_utilisateur,compteur[1]])
             compteur = [0,0]
-            L_validation += [numero_utilisateur,compteur[1]]
             
     return [T_validation,L_validation]
             
@@ -40,38 +57,42 @@ def notes_extraites_pour_test(tableau_des_notes_validation,tableau_des_notes_ent
     
     for n_eme_note_de_validation in range (9922):
         numero_utilisateur = randrange (0,670)
-        numero_film = randrange (1,2391)
+        while nbre_de_film_vu_par_utilisateur(tableau_des_notes_validation,numero_utilisateur) < 10: 
+            numero_utilisateur = randrange (0,670) 
+        numero_film = randrange (1,9125) 
         compteur = [0,0]
-        for i in range(len (T_validation[numero_utilisateur])):
-            if not math.isnan (T_validation[numero_utilisateur][i]):
+        for i, film in enumerate(T_validation[numero_utilisateur]): 
+            if not math.isnan (film): 
                 compteur = [compteur[0]+1,i]
                 if compteur[0] == numero_film:
                     T_test[numero_utilisateur][i] = na_n
                     compteur = [0,0]
-                    L_test += [numero_utilisateur,i]
+                    L_test.append([numero_utilisateur,i]) 
                     break
         if compteur[0] != 0:
             T_test[numero_utilisateur][compteur[1]] = na_n
+            L_test.append([numero_utilisateur,compteur[1]])
             compteur = [0,0]
-            L_test += [numero_utilisateur,compteur[1]]
             
     return [T_test,L_test]
 
-def ecart_moyen(L_notes_enlevees,Y_predit,tableau_des_notes_entier):
+def ecart_quadratique(L_notes_enlevees,Y_predit,tableau_des_notes_entier): 
     somme = 0
     for note in L_notes_enlevees:
-        somme+= abs(Y_predit[note[0]][note[1]]-tableau_des_notes_entier[note[0]][note[1]])
+        somme+= (Y_predit[note[0]][note[1]]-tableau_des_notes_entier[note[0]][note[1]])**2
     
     return somme/9922
     
 def nb_car_optimal():
     L_ecarts = []
     n = notes_extraites_pour_validation(tableau_des_notes())
-    for puissance in range (0,8):
-        d = descente_du_gradient(n[0],2**puissance,5000,0.001, 0.0001)
-        ecart = ecart_moyen(n[1],np.dot(d[0],(d[1]).T),tableau_des_notes)
+    i=2**3 
+    while i!=2**5: 
+        d = descente_du_gradient(n[0],i,500,0.001, 0.0001) 
+        ecart = ecart_quadratique(n[1],np.dot(d[1],(d[0]).T),tableau_des_notes())
         print(ecart)
-        L_ecarts += [2**puissance,ecart]
+        L_ecarts += [i,ecart] 
+        i+=4 
     
     return L_ecarts
 
